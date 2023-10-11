afb258d612e28c085e392bf06d468c95e962d8a1
groovinflannel <groovinflannel@gmail.com>
2023-10-11 14:22:54 -0400

    added turn_counter_clockwise and turn_clockwise methods to main.py

--- main.py
+++ main.py
@@ -11,7 +11,17 @@ def move_forward():
 def move_backward():
     turt.backward(10)
 
+
+def turn_counter_clockwise():
+    turt.left(10)
+
+
+def turn_clockwise():
+    turt.right(10)
+
 screen.listen()
 screen.onkey(key="w", fun=move_forward)
 screen.onkey(key="s", fun=move_backward)
+screen.onkey(key="a", fun=turn_counter_clockwise)
+screen.onkey(key="d", fun=turn_clockwise)
 screen.exitonclick()
