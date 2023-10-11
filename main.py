32e44c21b1e33bbae386317dc380927689c7e656
groovinflannel <groovinflannel@gmail.com>
2023-10-11 14:19:41 -0400

    add move_backward method to main.py

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def move_forward():
     turt.forward(10)
 
 
+def move_backward():
+    turt.backward(10)
+
 screen.listen()
-screen.onkey(key="space", fun=move_forward)
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="s", fun=move_backward)
 screen.exitonclick()
